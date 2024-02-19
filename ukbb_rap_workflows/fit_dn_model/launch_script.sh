513d2d2a12ee68dfd997be8847b974f9d8267057
RossDeVito <rdevito@ucsd.edu>
2024-02-19 14:28:47 -0800

    Add GPU docker

--- ukbb_rap_workflows/fit_dn_model/launch_script.sh
+++ ukbb_rap_workflows/fit_dn_model/launch_script.sh
@@ -1,11 +1,17 @@
 # Launch Deep Null training
 
-MODEL_CONFIG=lin_reg_1
+MODEL_CONFIG=xgb_2
 CONFIG_DIR=model_configs
 
-COVAR_SET=age_sex
+# COVAR_SET=age_sex
+# COVAR_SET=age_sex_pc
+# COVAR_SET=age_sex_birth_coords
+COVAR_SET=age_sex_birth_coords_pc
 
-PHENO=standing_height_50
+# PHENO=standing_height_50
+# PHENO=body_fat_percentage_23099
+# PHENO=platelet_count_30080
+PHENO=glycated_haemoglobin_30750
 
 VERSION=V2
 INSTANCE_TYPE=mem1_ssd1_v2_x16
